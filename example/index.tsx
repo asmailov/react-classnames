674bf5032fd318377b68602056f085ac689b538f
Aleksandr Šmailov <asmailov.reg@gmail.com>
2018-11-17 18:33:47 +0200

    Updated webpack to use umd target

--- example/index.tsx
+++ example/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ClassName } from '../src/core/component';
 
 const Index = () => {
-    return <div>react-classnames</div>;
+    return (
+        <ClassName className={['test', 'hello world', { asd: true }]}>
+            {({ className }) => (
+                <div className={className}>react-classnames</div>
+            )}
+        </ClassName>
+    );
 };
 
 ReactDOM.render(<Index />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,63 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const isProd = process.env.NODE_ENV !== 'production';
-const isDev = !isProd;
+module.exports = (_, { mode }) => {
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
 
-module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        loader: 'tslint-loader',
-                        options: {
-                            emitErrors: true,
+    return {
+        entry: isProd ? './src/index.tsx' : './example/index.tsx',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
+            libraryTarget: 'umd',
+            library: "ReactClassnames",
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.jsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    enforce: 'pre',
+                    use: [
+                        {
+                            loader: 'tslint-loader',
+                            options: {
+                                emitErrors: true,
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ],
-            },
+                    ]
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new ForkTsCheckerWebpackPlugin()
         ],
-    },
-    plugins: [
-        new ForkTsCheckerWebpackPlugin()
-    ],
-    externals: isProd ? {
-        react: 'React',
-        'react-dom': 'ReactDOM'
-    } : {},
-    devtool: isDev && 'source-map'
-};
+        externals: {
+            react: {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            },
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom'
+            }
+        },
+        devtool: isDev && 'source-map'
+    };
+}
